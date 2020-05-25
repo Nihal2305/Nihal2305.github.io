9ec634113c3db9ac22e5c9bf63611cf1483939db
Nihal2305 <nihalmaharaj2305@gmail.com>
2020-05-25 12:44:09 +0530

    Test commit before deploying

---
+++ build/precache-manifest.ad93e7390b07565081668ebe5b33ac31.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2ab65a8ee8a52cc016b252668577cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c0325bbc42d2011e01a",
+    "url": "/static/css/13.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "a6fef9b97f70c085536e",
+    "url": "/static/css/main.c578198d.chunk.css"
+  },
+  {
+    "revision": "1299115ebdcdd5797525",
+    "url": "/static/js/0.4beb3d1d.chunk.js"
+  },
+  {
+    "revision": "9c6305d7ad905d7fd96a",
+    "url": "/static/js/1.c2523961.chunk.js"
+  },
+  {
+    "revision": "a622e0a200ccebcda210",
+    "url": "/static/js/11.81c9a76c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/11.81c9a76c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6292f99e2ff33fe3c9c9",
+    "url": "/static/js/12.c94d0137.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/static/js/12.c94d0137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c0325bbc42d2011e01a",
+    "url": "/static/js/13.9ef44034.chunk.js"
+  },
+  {
+    "revision": "99e8c14132ad3ae86490",
+    "url": "/static/js/14.d0652d19.chunk.js"
+  },
+  {
+    "revision": "4c011fe126b656013f3fc475e891f49c",
+    "url": "/static/js/14.d0652d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e75f43cb9346852706f",
+    "url": "/static/js/15.f3d7bed5.chunk.js"
+  },
+  {
+    "revision": "aa4221ee53eea18b4b4fe4e3715ef01e",
+    "url": "/static/js/15.f3d7bed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a571c81c641950f8e71a",
+    "url": "/static/js/2.7b352a67.chunk.js"
+  },
+  {
+    "revision": "e1be9533748662fdb091",
+    "url": "/static/js/DeepDive.06fa97af.chunk.js"
+  },
+  {
+    "revision": "d017f7b8d36395917bc5",
+    "url": "/static/js/Essentials.5f3fe121.chunk.js"
+  },
+  {
+    "revision": "cdd652f9aa5b338590f9",
+    "url": "/static/js/FAQ.3acccc9b.chunk.js"
+  },
+  {
+    "revision": "08f394fe566d15c35317",
+    "url": "/static/js/Home.d0754bd3.chunk.js"
+  },
+  {
+    "revision": "eea81746c81f6a25552c",
+    "url": "/static/js/PatientDB.4e5e0f16.chunk.js"
+  },
+  {
+    "revision": "3dcb3c26db192f80c5c2",
+    "url": "/static/js/State.94b836b1.chunk.js"
+  },
+  {
+    "revision": "a6fef9b97f70c085536e",
+    "url": "/static/js/main.2b48d5c1.chunk.js"
+  },
+  {
+    "revision": "550fa52161967ac57a3f",
+    "url": "/static/js/runtime-main.01b48573.js"
+  },
+  {
+    "revision": "5e98861f6ef1ce225121abfb3f418166",
+    "url": "/static/media/getFetch.5e98861f.cjs"
+  }
+]);
